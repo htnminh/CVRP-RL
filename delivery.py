c03db00fc2b378ad94a12c94d49bfdf8dd1f4487
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2024-01-14 19:24:01 +0700

    fixed minor issues: the reward function + solution viz

--- delivery.py
+++ delivery.py
@@ -1,10 +1,13 @@
 from pprint import pprint, pformat
 from typing import Any, SupportsFloat
-import tqdm
 import time
+import warnings
+
+import tqdm
 
 import numpy as np
 from numpy.random import randint
+
 from scipy.spatial.distance import cdist, euclidean
 
 import matplotlib.pyplot as plt
@@ -25,8 +28,18 @@ from torch import tensor
 
 class Delivery(gymnasium.Env):
     def __init__(self, n_stops=10, max_demand=10, max_vehicle_cap=30, max_env_size=1_000_000,
-                 gen_seed=None, gym_seed=None) -> None:
+                 gen_seed=None, gym_seed=None, print_input=True) -> None:
+        # Print input info
+        if print_input:
+            print('n_stops:', n_stops)
+            print('max_demand:', max_demand)
+            print('max_vehicle_cap:', max_vehicle_cap)
+            print('max_env_size:', max_env_size)
+            print('gen_seed:', gen_seed)
+            print('gym_seed:', gym_seed)
+
         # Data generation
+        assert n_stops > 1 and max_demand > 0 and max_vehicle_cap > 0 and max_env_size > 0 
         self.n_stops = n_stops
         self.max_demand = max_demand
         self.max_vehicle_cap = max_vehicle_cap
@@ -40,7 +53,6 @@ class Delivery(gymnasium.Env):
         # Gymnasium env
         self.observation, self.info = self.reset()
 
-    
     def _generate_demands_and_vehicle_caps(self):
         demands = np.append([0], randint(1, self.max_demand + 1, size=self.n_stops - 1))
 
@@ -211,7 +223,7 @@ class Delivery(gymnasium.Env):
             route_coords = self.other_data['stops_coords'][[x['index'] for x in route_segments], :]
 
             ax1.plot(route_coords[:, 0], route_coords[:, 1], linewidth=1, color=_color)
-            ax1.annotate(f'{route_load}',
+            ax1.annotate(f'{route_distance} [{route_load}]',
                          ((route_coords[-1, 0] + route_coords[-2, 0])/2, (route_coords[-1, 1] + route_coords[-2, 1])/2),
                          fontsize=8, textcoords="offset points", xytext=(0, 0), ha='center', color=_color)
 
@@ -220,7 +232,12 @@ class Delivery(gymnasium.Env):
 
 
     def reset(self, seed=None):
-        # super().reset(seed=self.gym_seed)
+        try:
+            self.pre_reset_observation = self.observation
+            self.pre_reset_info = self.info   
+        except AttributeError:
+            self.pre_reset_observation = None
+            self.pre_reset_info = None
         
         # this seed is used to fix compatibility problems between gymnasium and stable-baselines3
         if seed is not None:
@@ -264,14 +281,36 @@ class Delivery(gymnasium.Env):
             current_length=np.array([0, ], dtype=int)
         )
         info['n_routes_redundant'] = 0
+        info['full_solution'] = []
+        info['ortools_format_solution'] = dict(
+            objective=0,
+            routes=[
+                dict(
+                    distance=0,
+                    segments=[dict(index=0, load=0), ]
+                )
+            ],
+            total_distance=0,
+            total_load=0
+        )
+        info['current_route_length'] = 0
+        info['total_reward'] = 0
 
         self.observation = observation
         self.info = info
+        
         
         return observation, info
     
 
     def step(self, action):
+        """
+        Note that the 'load' used in ortools is different from the 'load' used in gymnasium
+        - load of ortools is the total load that the vehicle has served
+        - load of gymnasium is the remaining load that the vehicle can serve
+            = vehicle_cap - load of ortools
+        except data in the ortools_format_solution
+        """
         assert self.action_space.contains(action), f"Invalid action {action}"
         self.info['n_routes_redundant'] += 1
 
@@ -282,37 +321,64 @@ class Delivery(gymnasium.Env):
             # print(f"Stop {action} has already been visited")
             # self.observation['current_length'] += 2 * self.max_env_size
             reward = INVALID_REWARD
+            self.info['total_reward'] += reward
             terminated = False
         elif self.observation['current_load'] < self.observation['demand'][action]:
             # print(f"Current load cannot be negative")
             # self.observation['current_length'] += 2 * self.max_env_size
             reward = INVALID_REWARD
+            self.info['total_reward'] += reward
             terminated = False
         elif self.observation['current_stop'] == 0 and action == 0:
             # print(f"Cannot stay in depot")
             # self.observation['current_length'] += 2 * self.max_env_size
             reward = INVALID_REWARD
+            self.info['total_reward'] += reward
             terminated = False
         else:
             # move to next stop
-            self.observation['current_length'] += int(euclidean(
+            # pprint(self.observation)
+            # pprint(self.info)
+            segment_length = int(euclidean(
                 self.observation['coord'][self.observation['current_stop']], self.observation['coord'][action]))
+            self.observation['current_length'] += segment_length
             if action != 0:
                 self.observation['visited'][action] = 1
             self.observation['current_load'] -= self.observation['demand'][action]
             self.observation['current_stop'] = action
+            self.info['full_solution'].append(action)
+            self.info['ortools_format_solution']['objective'] += segment_length
+            self.info['ortools_format_solution']['total_distance'] += segment_length
+            self.info['ortools_format_solution']['total_load'] += self.observation['demand'][action]
+            self.info['current_route_length'] += segment_length
+            self.info['ortools_format_solution']['routes'][-1]['distance'] += segment_length
+            self.info['ortools_format_solution']['routes'][-1]['segments'].append(dict(
+                index=int(action),
+                load=self.ortools_data['vehicle_caps'][0] - int(self.observation['current_load'])
+            ))
 
-            # reset load if back to depot
+            # reset if back to depot
             if self.observation['current_stop'] == 0:
                 self.observation['current_load'] = np.array([self.ortools_data['vehicle_caps'][0], ], dtype=int)
+                info['current_route_length'] = 0
+                self.info['ortools_format_solution']['routes'].append(
+                    dict(
+                        distance=0,
+                        segments=[dict(index=0, load=0), ]
+                    )
+                )
 
-            reward = -self.observation['current_length'] / self.max_env_size
-            # reward = self.observation['current_length']
+            # calc reward
+            reward = -segment_length / self.max_env_size
+            self.info['total_reward'] += reward
+            # reward = segment_length
 
             # check if done
             if np.sum(self.observation['visited']) == self.n_stops - 1 and self.observation['current_stop'] == 0:
-                print(f'TERMINATED: {self.observation["current_length"]} after {self.info["n_routes_redundant"]}')
+                print(f'TERMINATED:   length={self.observation["current_length"]}\t moves={self.info["n_routes_redundant"]}\t total_reward={self.info["total_reward"]}')
+                print('\t', [int(action) for action in self.info['full_solution']])
                 terminated = True
+                self.last_solution_info = self.info.copy()  # may raise an attribute error if no solution is found, which is rarely the case
                 self.reset()
             else:
                 terminated = False
@@ -333,13 +399,13 @@ class Delivery(gymnasium.Env):
             self.info
         )
     
+
     @staticmethod
     def convert_observation_to_tensor(observation: dict):
         res = observation.copy()
         for key in res.keys():
             res[key] = tensor(res[key])
         return res
-
 
 
 if __name__ == "__main__":
@@ -404,96 +470,181 @@ if __name__ == "__main__":
         observation, reward, terminated, truncated, info = delivery.step(action)
         if terminated or truncated:
             break
-"""
-    delivery = Delivery(n_stops=15, gen_seed=42, gym_seed=42, max_env_size=1000)
-    pprint(delivery.get_ortools_solution(time_limit_seconds=10))
-    input('Enter to continue...')
+    """
+    # initialization
+    N_STOPS = 15
+    N_MOVE_THRESHOLD = N_STOPS * 10
+
+    print('='*25 + 'Initialization' + '='*25)
+    delivery = Delivery(n_stops=N_STOPS, gen_seed=42, gym_seed=42, max_env_size=1000)
+    
+    # quick warning
+    warnings.warn(
+        """Sometimes an algorithm other than OR-Tools, typically PPO, may not be able to give a solution or take too long do so. """
+        """In that case, the solution will be fall back to its last found solution during training."""
+    )
+
+    # ortools
+    print('='*25 + 'OR-Tools' + '='*25)
+    pprint(ortools_solution := delivery.get_ortools_solution(time_limit_seconds=10))
+    delivery.visualize_ortools_solution(solution=ortools_solution)
+
+    """
+Solving with time limit of 10 seconds
+{'objective': 7104,
+ 'routes': [{'distance': 1459,
+             'segments': [{'index': 0, 'load': 0},
+                          {'index': 4, 'load': 6},
+                          {'index': 3, 'load': 16},
+                          {'index': 0, 'load': 16}]},
+            {'distance': 1654,
+             'segments': [{'index': 0, 'load': 0},
+                          {'index': 9, 'load': 7},
+                          {'index': 1, 'load': 12},
+                          {'index': 10, 'load': 16},
+                          {'index': 0, 'load': 16}]},
+            {'distance': 1340,
+             'segments': [{'index': 0, 'load': 0},
+                          {'index': 2, 'load': 1},
+                          {'index': 5, 'load': 10},
+                          {'index': 12, 'load': 13},
+                          {'index': 0, 'load': 13}]},
+            {'distance': 1214,
+             'segments': [{'index': 0, 'load': 0},
+                          {'index': 14, 'load': 3},
+                          {'index': 8, 'load': 6},
+                          {'index': 7, 'load': 16},
+                          {'index': 0, 'load': 16}]},
+            {'distance': 0,
+             'segments': [{'index': 0, 'load': 0}, {'index': 0, 'load': 0}]},
+            {'distance': 0,
+             'segments': [{'index': 0, 'load': 0}, {'index': 0, 'load': 0}]},
+            {'distance': 0,
+             'segments': [{'index': 0, 'load': 0}, {'index': 0, 'load': 0}]},
+            {'distance': 0,
+             'segments': [{'index': 0, 'load': 0}, {'index': 0, 'load': 0}]},
+            {'distance': 0,
+             'segments': [{'index': 0, 'load': 0}, {'index': 0, 'load': 0}]},
+            {'distance': 0,
+             'segments': [{'index': 0, 'load': 0}, {'index': 0, 'load': 0}]},
+            {'distance': 0,
+             'segments': [{'index': 0, 'load': 0}, {'index': 0, 'load': 0}]},
+            {'distance': 1437,
+             'segments': [{'index': 0, 'load': 0},
+                          {'index': 6, 'load': 1},
+                          {'index': 13, 'load': 6},
+                          {'index': 11, 'load': 15},
+                          {'index': 0, 'load': 15}]},
+            {'distance': 0,
+             'segments': [{'index': 0, 'load': 0}, {'index': 0, 'load': 0}]},
+            {'distance': 0,
+             'segments': [{'index': 0, 'load': 0}, {'index': 0, 'load': 0}]},
+            {'distance': 0,
+             'segments': [{'index': 0, 'load': 0}, {'index': 0, 'load': 0}]}],
+ 'total_distance': 7104,
+ 'total_load': 76}
+    """
 
     # a2c
+    print('='*25 + 'A2C' + '='*25)
     observation, info = delivery.reset()
     model = A2C("MultiInputPolicy", delivery, verbose=1)
     model.learn(total_timesteps=10_000, progress_bar=True)
+    last_solution_info = delivery.last_solution_info.copy()
     model.save("a2c_delivery")
     model = A2C.load("a2c_delivery")
-
+    
+    fallback = False
     observation, info = delivery.reset()
-    actions = []
     while True:
         action, _states = model.predict(observation, deterministic=False)
-        actions.append(action)
         observation, reward, terminated, truncated, info = delivery.step(action)
         # pprint(delivery.observation)
         if terminated or truncated:
             break
-    print([int(action) for action in actions])
-    
-    filtered_actions = []
-    for action in actions:
-        if action not in filtered_actions:
-            filtered_actions.append(action)
-        elif action == 0 and filtered_actions[-1] != 0:
-            filtered_actions.append(action)
-        
-    print([int(action) for action in filtered_actions])
-    time.sleep(3)
+        elif info["n_routes_redundant"] > N_MOVE_THRESHOLD:
+            warnings.warn(f'The algorithm took too long to give a solution, '
+                           'falling back to the last found solution during training.')
+            fallback = True
+            break
+
+    if not fallback:
+        final_info = delivery.pre_reset_info.copy()
+    else:
+        final_info = last_solution_info.copy()
+    print([int(action) for action in final_info['full_solution']])
+    pprint(final_info['ortools_format_solution'])
+    delivery.visualize_ortools_solution(solution=final_info['ortools_format_solution'])
     input('Enter to continue...')
 
 
     # dqn
+    print('='*25 + 'DQN' + '='*25)
     observation, info = delivery.reset()
     model = DQN("MultiInputPolicy", delivery, verbose=1, learning_rate=0.01)
-    model.learn(total_timesteps=100_000, progress_bar=True)
+    model.learn(total_timesteps=10_000, progress_bar=True)
+    last_solution_info = delivery.last_solution_info.copy()
     model.save("dqn_delivery")
     model = DQN.load("dqn_delivery")
 
+    fallback = False
     observation, info = delivery.reset()
-    actions = []
     while True:
         action, _states = model.predict(observation, deterministic=False)
-        actions.append(action)
         observation, reward, terminated, truncated, info = delivery.step(action)
         # pprint(delivery.observation)
         if terminated or truncated:
             break
-    print([int(action) for action in actions])
-    
-    filtered_actions = []
-    for action in actions:
-        if action not in filtered_actions:
-            filtered_actions.append(action)
-        elif action == 0 and filtered_actions[-1] != 0:
-            filtered_actions.append(action)
-        
-    print([int(action) for action in filtered_actions])
+        elif info["n_routes_redundant"] > N_MOVE_THRESHOLD:
+            warnings.warn(f'The algorithm took too long to give a solution, '
+                           'falling back to the last found solution during training.')
+            fallback = True
+            break
+
+    if not fallback:
+        final_info = delivery.pre_reset_info.copy()
+    else:
+        final_info = last_solution_info.copy()
+    print([int(action) for action in final_info['full_solution']])
+    pprint(final_info['ortools_format_solution'])
+    delivery.visualize_ortools_solution(solution=final_info['ortools_format_solution'])
     input('Enter to continue...')
 
 
     # ppo
+    # sometimes ppo does not give a solution at all
+    print('='*25 + 'PPO' + '='*25)
     observation, info = delivery.reset()
     model = PPO("MultiInputPolicy", delivery, verbose=1, learning_rate=0.01)
-    model.learn(total_timesteps=100_000, progress_bar=True)
+    model.learn(total_timesteps=10_000, progress_bar=True)
+    last_solution_info = delivery.last_solution_info.copy()
     model.save("ppo_delivery")
     model = PPO.load("ppo_delivery")
 
+    fallback = False
     observation, info = delivery.reset()
-    actions = []
     while True:
         action, _states = model.predict(observation, deterministic=False)
-        actions.append(action)
         observation, reward, terminated, truncated, info = delivery.step(action)
         # pprint(delivery.observation)
         if terminated or truncated:
             break
-    print([int(action) for action in actions])
+        elif info["n_routes_redundant"] > N_MOVE_THRESHOLD:
+            warnings.warn(f'The algorithm took too long to give a solution, '
+                           'falling back to the last found solution during training.')
+            fallback = True
+            break
 
-    filtered_actions = []
-    for action in actions:
-        if action not in filtered_actions:
-            filtered_actions.append(action)
-        elif action == 0 and filtered_actions[-1] != 0:
-            filtered_actions.append(action)
+    if not fallback:
+        final_info = delivery.pre_reset_info.copy()
+    else:
+        final_info = last_solution_info.copy()
+    print([int(action) for action in final_info['full_solution']])
+    pprint(final_info['ortools_format_solution'])
+    delivery.visualize_ortools_solution(solution=final_info['ortools_format_solution'])
+    input('Enter to continue...')
 
-    print([int(action) for action in filtered_actions])
+
 
 
 
