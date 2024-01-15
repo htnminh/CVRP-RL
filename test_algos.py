6a9c98e38cd6361d82967395fcb54af475b31070
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2024-01-15 17:02:39 +0700

    start test

--- test_algos.py
+++ test_algos.py
@@ -61,10 +61,10 @@ if __name__ == '__main__':
     )
     model_names = [model_name for _, model_name in models]
 
-    # TOTAL_TIMESTEPS_LIST = [250, 500, 1_000, 5_000, 10_000, 50_000, 100_000]
-    TOTAL_TIMESTEPS_LIST = [250, 500]  # test
-    # N_STOPS_LIST = [5, 10, 15, 20, 40, 50, 100]
-    N_STOPS_LIST = [5, 10]  # test
+    TOTAL_TIMESTEPS_LIST = [250, 500, 1_000, 5_000, 10_000, 50_000, 100_000]
+    # TOTAL_TIMESTEPS_LIST = [250, 500]  # test
+    N_STOPS_LIST = [5, 10, 15, 20, 40, 50, 100]
+    # N_STOPS_LIST = [5, 10]  # test
 
     # train and save results of each model
     for model, model_name in models:
